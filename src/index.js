49439622be6273cb74be44f687b630cab31f0042
skayer81 <114329023+skayer81@users.noreply.github.com>
2023-01-25 20:10:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  
+  let  openBrackets = '',
+        closeBrackets = '';
+  bracketsConfig.forEach(element => {
+    openBrackets += element[0];
+    closeBrackets += element[1]
+  });    
+
+  let i = 1;
+  
+  while(str != ''){
+    if (closeBrackets.includes(str[i])){
+      if (str[i-1] === openBrackets[closeBrackets.indexOf(str[i])]){
+        str = str.slice(0, i-1) + str.slice(i+1);
+        i -= 2;
+      }
+    } 
+    if (i > str.length) return false;
+    i++;
+  }   
+  return true;
+} 
